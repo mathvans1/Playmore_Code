f74250babbaa5b2011602a0868b97b20254144f6
Mathias Van Sele <mathvans1@student.arteveldehs.be>
2016-12-12 18:21:55 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ $(document).ready(function(){
 });
 /*Speelterein**************************************************************************************/
 
-
+var speelterrein;
 ready(function(){
 
     var App = {
@@ -187,7 +187,7 @@ ready(function(){
 
 
 /******************************************************Google maps**********************************/
-
+console.log(speelterrein);
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map-canvas'), {
@@ -196,23 +196,4 @@ function initMap() {
         mapTypeId: 'terrain'
     });
 
-    // Create a <script> tag and set the USGS URL as the source.
-    var script = document.createElement('script');
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-    for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1],coords[0]);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-        });
-    }
 }
