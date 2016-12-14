ac1666a218c11936ecdabb79b2bc4346be75ac8a
Jonatan Moerman <jonatan.moerman@student.arteveldehs.be>
2016-12-14 15:54:22 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -350,8 +350,15 @@ function initMap() {
 
 /******************************************************Like Button**********************************/
 
-var likes = 0;
-function onClick() {
-    likes += 1;
-    document.getElementById("likes").innerHTML = likes;
-};
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
